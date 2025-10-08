137f2efad2a2718ef7bfe3a68f467ac5d6a413c8
pekachoo <jliu6162@gmail.com>
2025-10-08 19:05:19 -0400

    just trying to get urdf to work

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pybullet as p
-import pybullet_data
 import time
+import pybullet_data
 
-physicsClient = p.connect(p.GUI)
-
-p.loadURDF("plane.urdf")
-
-p.setGravity(0, 0, -9.8)
-
-time.sleep(5)
-
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+startPos = [0,0,1]
+startOrientation = p.getQuaternionFromEuler([0,0,0])
+boxId = p.loadURDF("/Users/jliu61/Documents/GitHub/PyBullet-QLearning/urdf/box_diffdrive_pybullet.urdf",startPos, startOrientation)
+#set the center of mass frame (loadURDF sets base link frame)
+# startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos,startOrientation)
+for i in range (10000):
+    p.stepSimulation()
+    time.sleep(1./240.)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print(cubePos,cubeOrn)
 p.disconnect()
